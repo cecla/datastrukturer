1d61a7955d6c95ba1d806f4f3dff9a492bac2b79
alundin <alilu684@student.liu.se>
2015-05-26 11:10:16 +0200

    implemented uwsssp function and printPath

--- labb4a/digraph.cpp
+++ labb4a/digraph.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cassert>
+#include <vector>
 
 using namespace std;
 
@@ -65,8 +66,47 @@ void Digraph::uwsssp(int s)
          cout << "\nERROR: expected source s in range 1.." << size << " !" << endl;
          return;
     }
+    Queue<int> Q;
 
-    // *** TODO ***
+    // for all vertexes v in the graph, set dist(v) to INFINITY and path(v) to 0
+    for(int i = 1; i <= size; i++)
+    {
+        dist[i] = INFINITY;
+        path[i] = 0;
+    }
+    dist[s] = 0;
+
+    Q.enqueue(s);
+
+    while(!Q.isEmpty())
+    {
+        // fetch the first element in the queue and remove it from queue
+        int v = Q.getFront();
+        Q.dequeue();
+
+        // Get the first vertex pointed to by v
+        Node* p = array[v].getFirst();
+
+        // for every vertex adjacent to v,
+        // visit and check if it has been visited before        while(p)
+        {
+            cout << "Vertex " << v << " is pointing to " << p->vertex << endl;
+
+            // If dist is inf, the node has not been visited
+            if(dist[p->vertex] == INFINITY)
+            {
+                // set the dist to one more than the vertex it came from, v
+                dist[p->vertex] = dist[v]+1;
+                // path is the vertex it came from, v
+                path[p->vertex] = v;
+                // put it on the queue to later visit its neighbors too
+                Q.enqueue(p->vertex);
+            }
+            // move to the next vertex pointed to by v            p = array[v].getNext();
+        }
+        // when all of the vertexes pointed by v have been visited, got a correct distance and path
+        // and been put on the queue, go to the next on the queue and do the same there.
+    }
 }
 
 // positive weighted single source shortest pats
@@ -120,6 +160,38 @@ void Digraph::printPath(int t) const
          cout << "\nERROR: expected target t in range 1.." << size << " !" << endl;
          return;
     }
+    // look for the source value, which is where dist = 0
+    int source;
+    for(int i = 1; i <= size; i++)
+    {
+        cout << i << " dist = " << dist[i] << endl;
+        if(dist[i] == 0)
+        {
+            source = i;
+            break;
+        }
+    }
+    // use a vector to get the path in the right direction,
+    // which is the opposite of the way we receive them
+    vector<int> toPrint;
+    // start the path with the source
+    toPrint.push_back(source);
 
-    // *** TODO ***
+    // create temp int to not have to modify the target value
+    int temp = t;
+    // loop while the path have not reached the source, starting from the target
+    while(path[temp] != source)
+    {
+        // save the path and advance to the next target
+        toPrint.push_back(path[temp]);        temp = path[temp];
+    }
+
+    // use an iterator to go through the vector and display the path
+    vector<int>::iterator it = toPrint.begin();
+    while(it != toPrint.end())
+    {
+        cout<< " "<<*it;
+        it++;
+    }
+    cout << " (" << t << ")";
 }
